be512c63dc0de2be4a70e2629cb1c5df43fea00f
godls20455 <godls20455@ajou.ac.kr>
2024-03-08 17:57:01 +0900

    수정본 저장

--- reCalculator Lv.3/app/src/main/java/com/example/recalculator/Calculator.kt
+++
@@ -1,6 +0,0 @@
-package com.example.recalculator
-
-open class Calculator {
-    open fun calculate(num1:Int, num2:Int) {
-    }
-}
--- reCalculator Lv.3/app/src/main/java/com/example/recalculator/SubstractOperation.kt
+++
@@ -1,8 +0,0 @@
-package com.example.recalculator
-
-class SubstractOperation: Calculator() {
-    override fun calculate(num1:Int, num2:Int) {
-        var answer = num1 - num2
-        println("결과값은 $answer 입니다")
-    }
-}
