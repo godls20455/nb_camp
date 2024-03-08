58b8475d0fb49aa16015e4c0c3d80c3a9f8139a1
godls20455 <godls20455@ajou.ac.kr>
2024-03-08 17:44:41 +0900

    수정본 저장

---
+++ reCalculator Lv.3/app/src/main/java/com/example/recalculator/Calculator.kt
@@ -0,0 +1,6 @@
+package com.example.recalculator
+
+open class Calculator {
+    open fun calculate(num1:Int, num2:Int) {
+    }
+}
---
+++ reCalculator Lv.3/app/src/main/java/com/example/recalculator/DivideOperation.kt
@@ -0,0 +1,8 @@
+package com.example.recalculator
+
+class DivideOperation: Calculator() {
+    override fun calculate(num1: Int, num2: Int) {
+        var answer = num1 / num2
+        println("결과값은 $answer 입니다")
+    }
+}
